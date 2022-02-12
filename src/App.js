9e0e18c7b91263aeae4d75bcb4d69ecf079acd61
12nasir <nasirahmed8202@gmail.com>
2022-02-12 09:35:19 +0600

    inventory commit added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
@@ -6,7 +7,13 @@ function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+        <Route path="/">
+        <Shop></Shop>
+        </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
